a767886aeaed7f0fbeb3add383c78b93613778d0
Viktor Přehnal <viktor.prehnal@gmail.com>
2026-03-06 22:48:20 +0100

    WordPress Tests

--- bootstrapper/wp-test.bat
+++ bootstrapper/wp-test.bat
@@ -1,2 +1,8 @@
 @echo off
-docker exec -it 5e45d594ec37 /var/www/html/wp-content/plugins/src/vendor/bin/phpunit -c wp-content/plugins/src/phpunit.xml.dist
+FOR /F "usebackq delims=" %%A IN (`docker ps -q  --filter "name=tests-cli"`) DO (
+    set "VYSTUP=%%A"
+    goto :done
+)
+:done
+
+docker exec -it %VYSTUP% /var/www/html/wp-content/plugins/src/vendor/bin/phpunit -c wp-content/plugins/src/phpunit.xml.dist
