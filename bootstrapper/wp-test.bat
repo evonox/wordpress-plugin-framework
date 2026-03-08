d8c33f669c1c7e40be4335184cc60aff6964d0d5
Viktor Přehnal <viktor.prehnal@gmail.com>
2026-03-08 16:29:57 +0100

    FIX: wp-tests.bat

--- bootstrapper/wp-test.bat
+++ bootstrapper/wp-test.bat
@@ -5,7 +5,10 @@ FOR /F "usebackq delims=" %%A IN (`docker ps -q  --filter "name=tests-cli"`) DO 
 )
 :done
 
-set "arg=%~1"
-set "arg=%arg:\=/%"
-
-docker exec -it %VYSTUP% /var/www/html/wp-content/plugins/src/vendor/bin/phpunit -c wp-content/plugins/src/phpunit.xml.dist wp-content/plugins/src/%arg%
+if "%~1"=="" (
+    set "arg=%~1"
+    set "arg=%arg:\=/%"
+    docker exec -it %VYSTUP% /var/www/html/wp-content/plugins/src/vendor/bin/phpunit -c wp-content/plugins/src/phpunit.xml.dist wp-content/plugins/src/%arg%
+) else (
+    docker exec -it %VYSTUP% /var/www/html/wp-content/plugins/src/vendor/bin/phpunit -c wp-content/plugins/src/phpunit.xml.dist
+)
